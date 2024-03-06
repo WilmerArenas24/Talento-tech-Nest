8c1668aff355231576e90c1bf033774c65607d27
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-05 20:48:00 -0500

    Corrigiendo la conexion a la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); //cargamos las variables de configuracion
 
 @Module({
-  imports: [
-    UsersModule,
-    MongooseModule.forRoot(
-      process.env.DB_URL,
-    ),
-  ],
+  imports: [UsersModule,MongooseModule.forRoot(process.env.DB_URL)],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
+
 
 async function bootstrap() {
-  dotenv.config(); //cargamos las variables de configuracion
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
