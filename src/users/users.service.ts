0dc34d60b1d0c5fbbb3883481d62bc7e965f92a8
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-10 19:11:57 -0500

    implementando excepciones y validaciones

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { User } from './users.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcryptjs';
-import { promises } from 'dns';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
@@ -24,22 +28,41 @@ export class UsersService {
   }
 
   async findOne(id: string): Promise<User> {
-    return this.userModel.findById(id);
+    try {
+      return await this.userModel.findById(id);
+    } catch (error) {
+      throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
+    }
   }
-  
+
   async update(id: string, userDto: CreateUserDto): Promise<User> {
-    return this.userModel.findByIdAndUpdate(id, userDto,{ new:true});
+    if (userDto.password) {
+      const hashedPassword = await bcrypt.hash(userDto.password, 10);
+      userDto = {
+        ...userDto,
+        password: hashedPassword,
+      };
+    }
+    try {
+      return await this.userModel.findByIdAndUpdate(id, userDto, { new: true });
+    } catch (error) {
+      throw new NotFoundException('User not found');
+    }
   }
 
-  async delete(id: string): Promise<Boolean>{
- try {
-   await this.userModel.findByIdAndDelete(id);
-   return true;
-  
- } catch (error) {
-  throw new NotFoundException('User not found');
-  
- }
+  async delete(id: string): Promise<boolean> {
+    try {
+      const user = await this.userModel.findByIdAndDelete(id);
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+      return true;
+    } catch (error) {
+      throw new NotFoundException('User not found');
+    }
+  }
 
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.userModel.findOne({ email });
   }
 }
