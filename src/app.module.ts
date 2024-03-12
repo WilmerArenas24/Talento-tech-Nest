d694c82734e8e8a509176d7a2c8ad12988692ce9
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-12 17:29:34 -0500

    creando el modulo de houses

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HousesController } from './houses/houses.controller';
 import { HousesService } from './houses/houses.service';
+import { HousesModule } from './houses/houses.module';
 
 import * as dotenv from 'dotenv';
 
 dotenv.config(); //Cargamos las variables de configuracion
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot(process.env.DB_URL)],
+  imports: [UsersModule, MongooseModule.forRoot(process.env.DB_URL), HousesModule],
   controllers: [AppController, HousesController],
   providers: [AppService, HousesService],
 })
