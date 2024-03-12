a7ae2b7d9418a834332bbab31136edb5245bfe22
Wilmer Arenas <wilmerarenas24@gmail.com>
2024-03-12 17:51:47 -0500

    Modificando el modulo de houses

--- src/houses/houses.module.ts
+++ src/houses/houses.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HousesController } from './houses.controller';
+import { HousesService } from './houses.service';
 
-@Module({})
+@Module({
+    imports: [HousesModule],
+    controllers: [HousesController],
+    providers:[HousesService]
+})
 export class HousesModule {}
